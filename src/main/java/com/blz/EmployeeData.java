ecad66b03003b630cd2250358975890c32c27798
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-03-18 19:14:20 +0530

    Added: Create employee pay roll service logic

--- src/main/java/com/blz/EmployeeData.java
+++ src/main/java/com/blz/EmployeeData.java
@@ -1,5 +1,20 @@
 package com.blz;
 
 public class EmployeeData {
+	private int employeeId;
+	private String employeeName;
+	private double employeeSalary;
 
+	public EmployeeData(int employeeId, String employeeName, double employeeSalary) {
+		super();
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+		this.employeeSalary = employeeSalary;
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeeData [employeeId=" + employeeId + ", employeeName=" + employeeName + ", employeeSalary="
+				+ employeeSalary + "]";
+	}
 }
--- src/main/java/com/blz/EmployeePayrollService.java
+++ src/main/java/com/blz/EmployeePayrollService.java
@@ -1,8 +1,39 @@
 package com.blz;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class EmployeePayrollService {
 
+	private List<EmployeeData> employePayrollList = new ArrayList<EmployeeData>();
+	Scanner scanner = new Scanner(System.in);
+
+	/*
+	 * @purpose: This is the method to read the employee data from console.
+	 */
+	public void readEmployeeDataFromConsole() {
+		System.out.println("Enter Employee Id");
+		int id = scanner.nextInt();
+		System.out.println("Enter Employee Name");
+		String Name = scanner.next();
+		System.out.println("Enter the salary");
+		int salary = scanner.nextInt();
+		employePayrollList.add(new EmployeeData(id, Name, salary));
+	}
+
+	/*
+	 * @purpose: This is the method to write the employee data in console.
+	 */
+	public void writeEmployeeDataInConsole() {
+		System.out.println("Employee PayRoll Data \n" + employePayrollList);
+	}
+
+	// main
 	public static void main(String[] args) {
 		System.out.println("..WELCOME TO EMPLOYEE PAYROLL SYSTEM..");
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		employeePayrollService.readEmployeeDataFromConsole();
+		employeePayrollService.writeEmployeeDataInConsole();
 	}
 }
